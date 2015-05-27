610590cef0c808d48b265153e5b8d97043d8ee0c
Brian Cordonnier <bcordonn@gmail.com>
2015-05-26 21:30:48 -0500

    flip_dict change

--- flip_dict.py
+++ flip_dict.py
@@ -18,6 +18,17 @@ def read_files(old_path, new_path):
             str = json.dumps(new_dict, ensure_ascii=False, sort_keys=True, indent=4, separators=(',', ': '))
             f.write(str.encode('UTF-8'))
 
+def curl_generate():
+    #add a comment
+    with open(READ_FILE, 'r') as f:
+        lines = f.readlines()
+        lines = [s.replace("\n", "") for s in lines]
+        final = " ".join(lines)
+        with open('curl_parse_run.sh', 'w') as f2:
+            f2.write(final)
+    st = os.stat(RUN_FILE)
+    os.chmod(RUN_FILE, st.st_mode | stat.S_IEXEC)
+
 if __name__ == '__main__':
     old_path = "/Users/briancordonnier/repos/sandbox/new_target"
     new_path = "/Users/briancordonnier/repos/sandbox/target_fliped"
